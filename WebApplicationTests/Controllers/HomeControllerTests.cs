22cdff9beb16eb47cd0eec0055796e5f8eb82e07
Carolini28 <carolinirodrigues28@gmail.com>
2020-11-15 16:09:19 -0400

    testes

---
+++ WebApplicationTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebApplication.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication.Controllers.Tests {
+	[TestClass()]
+	public class HomeControllerTests {
+		[TestMethod()]
+		public void IndexTest()
+		{
+			Assert.Fail();
+		}
+	}
+}
